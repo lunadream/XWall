bddf445276d249b24051a7e3fc49c982c85c68f1
VILIC VANE <i@vilic.info>
2012-11-26 00:35:58 +0800

    move google analytics code position

--- scripts/main.js
+++ scripts/main.js
@@ -7,18 +7,6 @@ window.onload = function () {
 
     var main = import_("main");
     if (main) main.load();
-
-    //Google Analytics
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-36452018-1']);
-    _gaq.push(['_setDomainName', 'x-wall.org']);
-    _gaq.push(['_trackPageview']);
-
-    (function () {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
 };
 
 use_("dom", "dom.animation", function (dom, anim) {
