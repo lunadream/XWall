a555c2d4b9ae55b233e885b262e87b73b641d9ee
VILIC VANE <i@vilic.info>
2012-11-30 12:20:06 +0800

    small changes

--- X-Wall/Classes/Plink.cs
+++ X-Wall/Classes/Plink.cs
@@ -194,7 +194,7 @@ namespace XWall {
             else if (line.StartsWith("Nothing left to send, closing channel"))
                 portCloseCount = Math.Min(1, portCloseCount + 1);
             else if (line.StartsWith("Forwarded port closed")) {
-                if (--portCloseCount < -settings.AbortionBeforeReconnect)
+                if (--portCloseCount < -settings.AbortionBeforeReconnect && settings.SshAutoReconnect)
                     Stop(true);
             }
             else if (line.StartsWith("The server's host key is not cached in the registry."))
