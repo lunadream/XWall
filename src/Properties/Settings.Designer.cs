c85d67beaa0eb17858eee2a4a46478ff149a15ea
VILIC VANE <i@vilic.info>
2014-01-10 01:00:49 +0800

    followup goagent updates

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -891,7 +891,7 @@ namespace XWall.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("4")]
+        [global::System.Configuration.DefaultSettingValueAttribute("5")]
         public int GaServerVersion {
             get {
                 return ((int)(this["GaServerVersion"]));
